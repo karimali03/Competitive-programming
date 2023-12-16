7e7c1a92defa836a6726c99661293c3e123784fb
Karim Ali <karimabdelaziz12703@gmail.com>
2023-12-16 21:27:32 +0200

    add sieve,fact,gcd,spf

---
+++ math/math.cpp
@@ -0,0 +1,139 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#define int long long 
+using namespace std;
+
+
+// get divisors of n
+vector<int> divisiors(int n){ // O( sqrt(n) )
+    vector<int>res;
+    for(int i=1;i*i<=n;i++){
+        if(n%i==0){
+            res.push_back(i);
+            if(i!=n/i) res.push_back(n/i);
+        }
+    }
+    return res;
+}
+
+// check number is prime 
+bool isprime(int n){    // O( sqrt(n) )
+    if(n<=1) return false;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0) return false;
+    }
+    return true;
+}
+
+
+
+
+const int N=1e6+5;
+
+vector<int>prime(N,1);
+vector<int>primes;
+ void sieve(){    // Sieve algorithm O(n*log(log(n)))
+
+ for(int i=2;i*i<=N;i++){
+    if(prime[i])  
+	  for(int j=i*i;j<=N;j+=i)    prime[j]=0;    
+   }
+    for(int i=2;i<=N;i++) if(prime[i]) primes.push_back(i);   
+
+     }       
+
+// spf ( smallest prime factor )
+vector<int>spf(N);   
+ void SPF(){   //  O(n*log(log(n)))
+    for(int i=1;i<N;i++) spf[i]=i;
+
+     for(int i=2; i*i<=N; i++)
+    {
+        if(spf[i]==i)
+           for(int j=i*i; j<=N; j+=i)
+               if(spf[j]==j)   spf[j]=i;
+    }
+ }
+
+
+ map<int,int> fast_fact(int n){   // O ( log(n) )
+     map<int,int>res;
+     while (n>1)
+     {
+        res[spf[n]]++;
+        n/=spf[n];
+     }
+     return res;
+     
+ }
+ 
+
+ map<int,int> fact(int n){  // O( sqrt(n) )
+    map<int,int>res;
+    for(int i=2;i*i<=n;i++){
+        while (n%i==0)
+        {
+           res[i]++;
+           n/=i;
+        }
+    }
+    if(n!=1) res[n]++;
+    return res;
+
+ }
+
+
+ const int  mod = 1000000007;
+
+ int add(int a, int b) {
+    return ((a % mod) + (b % mod)) % mod;
+}
+ 
+int mul(int a, int b) {
+    return ((a % mod) * (b % mod)) % mod;
+}
+ 
+int sub(int a, int b) {
+    return ((a % mod) - (b % mod) + mod) % mod;
+}
+
+
+int GCD(int a,int b){
+    int mx=max(a,b);
+    int mn=min(a,b);
+
+    while (mn>0)
+    {
+        int temp = mn;   
+        mn=mx%mn; 
+        mx=temp;
+    }
+
+    return mx;
+}
+ 
+int LCM(int a,int b){
+    return a / GCD(a,b) * b;
+}
+
+
+
+ int fast_power(int a,int b){
+    int res=1;
+    while(b){
+        if(b%2) res*=a;
+         a*=a;
+         b/=2;
+    }
+
+   return res;
+}
+
+
+
+
+signed main(){
+    
+    
+    return 0;
+}
