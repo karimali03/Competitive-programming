587f4bbfc406802c8e6ef2237e7946739ac2b541
Karim Ali <ka996038@gmail.com>
2024-11-17 07:37:19 +0200

    edit sparse table

--- templetes/Data Structures/sparse_table.cpp
+++ templetes/Data Structures/sparse_table.cpp
@@ -2,31 +2,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX_N = 100'005;
-const int LOG = 17;
-int m[MAX_N][LOG]; // m[i][j] is minimum among a[i..i+2^j-1]
-int bin_log[MAX_N];
-
-
-void pre_calc(vector<int>&v){
-	int n = v.size();
-	bin_log[1] = bin_log[0] = 0;
-	for(int i = 2; i <= n; i++) {
-		bin_log[i] = bin_log[i/2]+1;
-	}
-	for(int i = 0; i < n; i++) {
-		m[i][0] = v[i];
-	}
-	// 2) preprocessing, O(N*log(N))
-	for(int k = 1; k < LOG; k++) {
-		for(int i = 0; i + (1 << k) - 1 < n; i++) {
-			m[i][k] = min(m[i][k-1], m[i+(1<<(k-1))][k-1]);
+struct sparse {
+	vector<vector<int>>m;
+	sparse(vector<int>&v){
+		int n = v.size();
+		m = vector<vector<int>>(n,vector<int>(20));
+			
+		for(int i = 0; i < n; i++) {
+			m[i][0] = v[i];
+		}
+		// 2) preprocessing, O(N*log(N))
+		for(int k = 1; k < 20; k++) {
+			for(int i = 0; i + (1 << k) - 1 < n; i++) {
+				m[i][k] = (m[i][k-1] + m[i+(1<<(k-1))][k-1]);
+			}
 		}
 	}
-}
-
-int query(int L, int R) { // O(1)
-	int length = R - L + 1;
-	int k = bin_log[length];
-	return min(m[L][k], m[R-(1<<k)+1][k]);
-}
+	
+	int query(int L, int R) { // O(1)
+		int len = R - L + 1;
+		int k = 31 - __builtin_clz(len);
+		return max(m[L][k], m[R-(1<<k)+1][k]);
+	}
+	
+	int query_sum(int L, int R) { // O(logn)
+		int sum = 0;
+		for( int i = L ;  i <= R ;){
+			int k = 31 - __builtin_clz(R-i+1);
+			sum+=m[i][k]; 
+			i+=(1<<k);
+			
+		}
+		return sum;
+	}
+	
+};
