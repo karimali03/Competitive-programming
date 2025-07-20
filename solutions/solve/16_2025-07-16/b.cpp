623e4423fb6d033993f7b1687c877950f6788d9b
Karim Ali <ka996038@gmail.com>
2025-07-20 14:02:17 +0300

    🔧 Update changes

---
+++ solutions/solve/16_2025-07-16/b.cpp
@@ -0,0 +1,128 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ln "\n"
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vec vector
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define viii vector<vector<vector<int>>>
+#define f(i, a, b) for(int i = a; i < b; i++)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define co(x) __builtin_popcountll(x)
+#define ctz(x) __builtin_ctzll(x)
+#define clz(x) __builtin_clzll(x)
+#define PI acos(-1)
+#define int long long
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+#define NA cout<<"-1\n"
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(ld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(map <T, V> v);
+
+template <class T, class V> void _print(pair <T, V> p) {
+    cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";
+}
+template <class T> void _print(vector <T> v) {
+    cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+template <class T> void _print(set <T> v) {
+    cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+template <class T> void _print(multiset <T> v) {
+    cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+template <class T, class V> void _print(map <T, V> v) {
+    cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+
+template<typename T = int>
+istream &operator>>(istream &in, vector<T> &v) {
+    for (auto &x : v) in >> x;
+    return in;
+}
+
+template<typename T = int>
+ostream &operator<<(ostream &out, const vector<T> &v) {
+    for (const T &x : v) out << x << ' ';
+    return out;
+}
+
+const int N = 500005;
+vector<pair<int,int>> adj[N];
+int vis[N];
+vi path;
+void dfs(int v,int p, stack<pair<int,int>>&st){
+    vis[v] = 1;
+    for(auto [u,e] : adj[v]){
+        if(vis[u]){
+            path.push_back(e);
+            while(!st.empty() && st.top().first != u){
+                path.push_back(st.top().second);
+                st.pop();
+            }
+            continue;
+        }
+        st.push({v,e});
+        dfs(u,v,st);
+        if(!st.empty()) st.pop();
+    }
+}
+
+void init(int n){
+    for(int i = 0 ; i < n ; i++){  
+        adj[i].clear();
+        vis[i] = 0;
+    }
+    path.clear();
+}
+void solve(int test_case) {
+    int n; cin>>n;
+    init(n);
+    for(int i = 0 ; i < n ; i++){
+        int x,y; cin>>x>>y; x--,y--;
+        adj[x].push_back({y,i});
+        adj[y].push_back({x,i});
+    }
+    stack<pair<int,int>>st;
+    dfs(0,-1,st);
+    int cnt = 0;
+    cout<<path<<ln;
+    int cc = 1;
+  
+    int res = 0;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    for (int i = 1; i <= t; i++) {
+        solve(i);
+    }
+
+    return 0;
+}
