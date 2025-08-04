ddcb2fc69d78683c1c67aa1103b8c7bf04866ecd
Karim <karim@192.168.1.11>
2025-08-04 13:37:07 +0300

    🔧 Update changes

--- solutions/solve/today_2025-07-26/a.cpp
+++ solutions/solve/today_2025-07-26/a.cpp
@@ -1,68 +1,212 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <climits>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int hammingDistance(const string &a, const string &b) {
-    int distance = 0;
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            distance++;
-        }
-    }
-    return distance;
+#define ln "\n"
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vec vector
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define viii vector<vector<vector<int>>>
+#define f(i, a, b) for(int i = a; i < b; i++)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define co(x) __builtin_popcountll(x)
+#define ctz(x) __builtin_ctzll(x)
+#define clz(x) __builtin_clzll(x)
+#define PI acos(-1)
+#define int long long
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+#define NA cout<<"-1\n"
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(ld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(map <T, V> v);
+
+template <class T, class V> void _print(pair <T, V> p) {
+    cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";
+}
+template <class T> void _print(vector <T> v) {
+    cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+template <class T> void _print(set <T> v) {
+    cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+template <class T> void _print(multiset <T> v) {
+    cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+template <class T, class V> void _print(map <T, V> v) {
+    cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";
 }
 
-string findBestString(int n, int k, const vector<string> &friends) {
-    string bestStr(k, '0');
-    int maxMinDistance = -1;
+template<typename T = int>
+istream &operator>>(istream &in, vector<T> &v) {
+    for (auto &x : v) in >> x;
+    return in;
+}
 
-    // Iterate all possible strings in lexicographical order
-    for (int mask = 0; mask < (1 << k); ++mask) {
-        string currentStr(k, '0');
-        for (int i = 0; i < k; ++i) {
-            if (mask & (1 << (k - 1 - i))) {
-                currentStr[i] = '1';
-            }
-        }
+template<typename T = int>
+ostream &operator<<(ostream &out, const vector<T> &v) {
+    for (const T &x : v) out << x << ' ';
+    return out;
+}
 
-        int minDistance = INT_MAX;
-        for (const string &friendStr : friends) {
-            int distance = hammingDistance(currentStr, friendStr);
-            if (distance < minDistance) {
-                minDistance = distance;
-                if (minDistance <= maxMinDistance) {
-                    break; // No need to check further for this string
+
+/*
+    1. General Graph (arbitrary capacities):
+       - Time: O(V^2 * E)
+    2. Unit Capacity Graphs (capacities = 1):
+       - Time: O(√V * E)
+    3. Bounded Small Capacities (e.g., 1 ≤ c ≤ 1000):
+       - Time: Better than general case, often around O(min(V^2, E√V))
+    - Dinic is best for sparse graphs or unit capacity cases
+    - For large capacities or dense graphs, use Push-Relabel or Capacity Scaling
+  */
+  
+const int MAXV = 1005;
+const int INF = 1e9;
+
+struct Dinic {
+    struct Edge {
+        int to, rev;
+        int cap, flow;
+    };
+
+    vector<Edge> adj[MAXV];
+    int level[MAXV], ptr[MAXV];
+    int s, t;
+
+    void init(int _s, int _t) {
+        s = _s;
+        t = _t;
+        for (int i = 0; i < MAXV; ++i)
+            adj[i].clear();
+    }
+
+    void addEdge(int a, int b, int cap, bool directed = true) {
+        adj[a].push_back({b, (int)adj[b].size(), cap, 0});
+        adj[b].push_back({a, (int)adj[a].size() - 1, directed ? 0 : cap, 0});
+    }
+
+    bool bfs() {
+        memset(level, -1, sizeof(level));
+        queue<int> q;
+        q.push(s);
+        level[s] = 0;
+        while (!q.empty()) {
+            int v = q.front(); q.pop();
+            for (auto &e : adj[v]) {
+                if (level[e.to] == -1 && e.flow < e.cap) {
+                    level[e.to] = level[v] + 1;
+                    q.push(e.to);
                 }
             }
         }
+        return level[t] != -1;
+    }
 
-        if (minDistance > maxMinDistance) {
-            maxMinDistance = minDistance;
-            bestStr = currentStr;
-        } else if (minDistance == maxMinDistance) {
-            if (currentStr < bestStr) {
-                bestStr = currentStr;
+    int dfs(int v, int pushed) {
+        if (pushed == 0) return 0;
+        if (v == t) return pushed;
+        for (int &i = ptr[v]; i < adj[v].size(); ++i) {
+            Edge &e = adj[v][i];
+            if (level[e.to] != level[v] + 1 || e.flow == e.cap) continue;
+            int tr = dfs(e.to, min(pushed, e.cap - e.flow));
+            if (tr == 0) continue;
+            e.flow += tr;
+            adj[e.to][e.rev].flow -= tr;
+            return tr;
+        }
+        return 0;
+    }
+
+    int maxFlow() {
+        int flow = 0;
+        while (bfs()) {
+            fill(ptr, ptr + MAXV, 0);
+            while (int pushed = dfs(s, INF)) {
+                flow += pushed;
             }
         }
+        return flow;
     }
 
-    return bestStr;
+    vector<vector<int>> paths;
+
+    bool dfs_path(int u, vector<int> &path) {
+        if (u == t) {
+            paths.push_back(path);
+            return true;
+        }
+        for (auto &e : adj[u]) {
+            if (e.cap > 0 && e.flow == e.cap) {
+                path.push_back(e.to);
+                e.flow = 0; // mark used
+                if (dfs_path(e.to, path)) return true;
+                path.pop_back();
+                e.flow = e.cap; // backtrack
+            }
+        }
+        return false;
+    }
+
+    vector<vector<int>> getAllFlowPaths() {
+        paths.clear();
+        while (true) {
+            vector<int> path = {s};
+            if (!dfs_path(s, path)) break;
+        }
+        return paths;
+    }
+
+    void printGraph(int n) {
+        cout << "Graph (Node -> [To, Cap, Flow]):\n";
+        for (int i = 0; i < n; ++i) {
+            if (adj[i].empty()) continue;
+            cout << "Node " << i << ": ";
+            for (const auto &e : adj[i]) {
+                if (e.cap > 0)
+                    cout << "[" << e.to << ", " << e.cap << ", " << e.flow << "] ";
+            }
+            cout << "\n";
+        }
+    }
+};
+
+
+void solve(int test_case) {
+    int n,m; cin>>n>>m;
+    vi v(n); cin>>v;
+    
 }
 
-int main() {
-    int n, k;
-    cin >> n >> k;
-    vector<string> friends(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> friends[i];
-    }
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-    string result = findBestString(n, k, friends);
-    cout << result << endl;
+    int t = 1;
+    cin >> t;
+    for (int i = 1; i <= t; i++) {
+        solve(i);
+    }
 
     return 0;
 }
