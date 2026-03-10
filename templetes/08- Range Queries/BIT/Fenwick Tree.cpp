aba162223969e3fa0b74967789c76203b828e957
Karim Ali Abdelaziz <ka996038@gmail.com>
2026-03-10 07:47:34 +0200

    🔧 Update changes

--- templetes/08- Range Queries/BIT/Fenwick Tree.cpp
+++ templetes/08- Range Queries/BIT/Fenwick Tree.cpp
@@ -3,19 +3,21 @@ struct Fenwick {
     vector<ll> bit;
     Fenwick() : n(0) {}
     Fenwick(int n_){
-        n = n_;
+        n = n_ + 5;
         bit.assign(n+1, 0); // 1-indexed, supports indices 1..n
     }
-    void add(int idx, ll val){ // idx is 1..n
+    // idx is 1..n
+    void add(int idx, ll val){ 
         while(idx <= n){
-            bit[idx] = (bit[idx] + val) % MOD;
+            bit[idx] = (bit[idx] + val);
             idx += idx & -idx;
         }
     }
-    ll sumPrefix(int idx){ // sum 1..idx
+    // sum 1..idx
+    ll sumPrefix(int idx){ 
         ll res = 0;
         while(idx > 0){
-            res = (res + bit[idx]) % MOD;
+            res = (res + bit[idx]);
             idx -= idx & -idx;
         }
         return res;
@@ -23,6 +25,9 @@ struct Fenwick {
     // sum on [l..r] with l,r in 1..n
     ll sumRange(int l, int r){
         if(l > r) return 0;
-        return (sumPrefix(r) - sumPrefix(l-1) + MOD) % MOD;
+        return (sumPrefix(r) - sumPrefix(l-1));
     }
+    // for range update , point query
+    // update [l,r] +x  -> add(l,x) , add(r+1,-x) 
+    // get idx  -> sumPrefix(idx)
 };
